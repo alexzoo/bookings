2d56405da0d014a9df8da354a0fac4410a879493
Alexey Zubov <zubov.alexey@gmail.com>
2024-01-31 15:32:21 +0100

    add fixture for create data

--- tests/test_get_bookings_ids.py
+++ tests/test_get_bookings_ids.py
@@ -1,6 +1,7 @@
 import pytest
 
-from models.booking_model import BookingID, BookingGetByIdResponse
+from models.booking_model import BookingID, BookingGetByIdResponse, BookingCreateRequest, BookingCreateResponse, \
+    BookingDates
 
 
 class TestGetBookingsIds:
@@ -13,20 +14,22 @@ class TestGetBookingsIds:
         for booking_id in response:
             BookingID(**booking_id)
 
-    @pytest.mark.with_error
-    def test_get_booking_by_id(self, bookings):
-        booking_id = 3
+    def test_get_booking_by_id(self, bookings, create_booking):
+        booking_id = create_booking.bookingid
+        firstname = create_booking.firstname
+        lastname = create_booking.lastname
         response = bookings.get_by_id(booking_id=booking_id)
 
         booking_response = BookingGetByIdResponse(**response)
-        assert isinstance(booking_response.firstname, str)
-        assert isinstance(booking_response.lastname, str)
+        assert booking_response.firstname == firstname
+        assert booking_response.lastname == lastname
 
-    @pytest.mark.with_error
-    def test_get_booking_by_name(self, bookings):
-        data = bookings.get_by_name('Alex', 'Zoo')
+    def test_get_booking_by_name(self, bookings, create_booking):
+        firstname = create_booking.firstname
+        lastname = create_booking.lastname
+        data = bookings.get_by_name(firstname, lastname)
 
-        assert len(data) > 0
+        assert data == ''
 
     @pytest.mark.smoke
     def test_get_bookings_by_checkin_date(self, bookings):
@@ -44,3 +47,19 @@ class TestGetBookingsIds:
     def test_cant_get_booking_by_incorrect_id(self, bookings):
         booking_id = 9999999
         bookings.get_by_id(booking_id, expected_status=404)
+
+
+@pytest.fixture(scope='function')
+def create_booking(bookings):
+    booking_data = BookingCreateRequest(firstname='Alex',
+                                        lastname='Zoo',
+                                        totalprice=111,
+                                        depositpaid=True,
+                                        bookingdates=BookingDates(checkin='2024-02-10',
+                                                                  checkout='2024-02-20'),
+                                        additionalneeds='Breakfast')
+    response = bookings.create(booking_data=booking_data.model_dump())
+    booking_response = BookingCreateResponse(**response)
+
+    yield booking_response
+    bookings.delete(booking_id=booking_data.bookingid)
