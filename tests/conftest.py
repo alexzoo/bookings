c2257f72a794d64d4b5312d01f2ab6bbb27aa128
Alexey Zubov <zubov.alexey@gmail.com>
2024-01-31 12:27:56 +0100

    remove fixture apiclient

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,7 @@
 import pytest
-
-from utils.api_client import ApiClient
 from services.booking_service import Bookings
 
 
 @pytest.fixture
-def api_client():
-    return ApiClient()
-
-
-@pytest.fixture
-def bookings(api_client):
-    return Bookings(api_client)
+def bookings():
+    return Bookings()
